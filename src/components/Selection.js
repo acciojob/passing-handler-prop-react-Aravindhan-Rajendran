5dbcb93dc1353b345a7a80610cd8622216f4651f
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-25 19:59:52 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,33 @@
+import React, { useState } from 'react';
+
+const Selection = (props) => {
+  const [background, setBackground] = useState('');
+
+  const { applyColor } = props;
+
+  const handleClick = () => {
+    applyColor(setBackground);
+  };
+
+  const boxStyle = {
+    width: '100px',
+    height: '100px',
+    border: '1px solid #ccc',
+    margin: '10px',
+    display: 'inline-block',
+    backgroundColor: background,
+    cursor: 'pointer'
+  };
+
+  return (
+    <div 
+      style={boxStyle}
+      onClick={handleClick}
+    >
+      {/* Content inside the box, if any */}
+    </div>
+  );
+}
+
+export default Selection;
 
